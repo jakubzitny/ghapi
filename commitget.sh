75d17f992082834b63123fe18c24544484922dd3
EC2 Default User <ec2-user@ip-172-31-13-121.eu-west-1.compute.internal>
2016-12-22 23:31:29 +0000

    commit fix

--- commitget.sh
+++ commitget.sh
@@ -9,17 +9,16 @@ list=data/"$1"
 
 
 getCommitResponse() {
-	curl -ni "$1/commits?access_token$ghtoken" -H 'Accept: application/vnd.github.preview' 2> /dev/null
+	curl -ni "$1/commits?access_token=$ghtoken" -H 'Accept: application/vnd.github.preview' 2> /dev/null
 }
 
 processUrl() {
 	url="$2"
 	id="$1"
 	commitresponse=`getCommitResponse $url`
-	commitPages=
-	redirect=`echo "$repojson" | grep '"message": "Moved Permanently"'`
-	notfound=`echo "$repojson" | grep '"message": "Not Found"'`
-	limit=`echo "$repojson" | grep '"message": "API rate limit'`
+	redirect=`echo "$commitresponse" | grep '"message": "Moved Permanently"'`
+	notfound=`echo "$commitresponse" | grep '"message": "Not Found"'`
+	limit=`echo "$commitresponse" | grep '"message": "API rate limit'`
 	if [ "$limit" ]; then
 		reset=`curl --silent "https://api.github.com/rate_limit?access_token=$ghtoken" -H 'Accept: application/vnd.github.preview' | jq '.resources.core.reset'`
 		now=`date +%s`
@@ -27,19 +26,20 @@ processUrl() {
 		sleep $toSleep
 		processUrl $id $url
 	elif [ "$redirect" ]; then
-		redirectUrl=`echo "$repojson" | jq '.url' | sed 's/\"//g' | cut -d"?" -f1`
+		redirectUrl=`echo "$commitresponse" | jq '.url' | sed 's/\"//g' | cut -d"?" -f1`
 		processUrl $id $redirectUrl
-		echo $id,$url "--" REDIRECT >> $prefix/redirects_$dataid.txt
+		echo $id,$url "--" REDIRECT >> $prefix/redirects_c_$dataid.txt
 	elif [ "$notfound" ]; then
-		echo $id,$url "--" NOTFOUND >> $prefix/notfounds_$dataid.txt
-	elif [ "$stargazers_fields_count" -gt "1" ]; then
-		echo $id,$url "--" MULTIPLE $stargazers_fields_count >> $prefix/multiples_$dataid.txt
+		echo $id,$url "--" NOTFOUND >> $prefix/notfounds_c_$dataid.txt
 	else
-		commitPages=`echo "$commitresponse" | head -26 | awk -F"=" '/Link:/{print $4}' | sed 's/\>.*$//g'`
+		commitPages=`echo "$commitresponse" | head -26 | awk -F">;" '/Link:/{print $2}' | sed 's/^.*=//g'`
 		commitCount=`echo $((commitPages*30))`
-		lastCommitDate=`echo "$commitresponse" | tail -n +27 | jq '.[1].commit.author.date'`
-		echo $id,$commitPages,$commitCount
-		echo $id,$commitPages,$commitCount >> $prefix/commits_$dataid.csv
+		commitJson=`echo "$commitresponse" | tail -n +28`
+		#echo "$commitresponse"
+		#echo "$commitJson"
+		lastCommitDate=`echo "$commitJson" | jq '.[1].commit.author.date'`
+		echo $id,$lastCommitDate,$commitCount
+		echo $id,$lastCommitDate,$commitCount >> $prefix/commits_$dataid.csv
 	fi
 }
 
